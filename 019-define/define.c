d710c0f63915aa885118d472e7cab0d4e5a75742
Luke Armstrong <me@lukearmstrong.co.uk>
2012-08-16 22:36:50 +0100

    Defining constants

---
+++ 019-define/define.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define LINE "_____________________________"
+#define TITLE "C Programming in easy steps"
+#define AUTHOR "Mike McGrath"
+
+int main()
+{
+	printf("\n \t %s \n \t %s \n", LINE, TITLE);
+	printf("\t by %s \n \t %s \n", AUTHOR, LINE);
+
+	return 0;
+}
